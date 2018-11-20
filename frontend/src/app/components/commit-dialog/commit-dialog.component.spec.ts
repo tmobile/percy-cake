320c889f11895cd466654a3b4625271b0d91d06a
liuliquan <leviastan@gmail.com>
2018-11-21 04:03:57 +0800

    commit dialog unit test

--- frontend/src/app/components/commit-dialog/commit-dialog.component.spec.ts
+++ frontend/src/app/components/commit-dialog/commit-dialog.component.spec.ts
@@ -9,4 +9,16 @@ describe('CommitDialogComponent', () => {
   it('should create CommitDialogComponent', () => {
     expect(ctx().component).toBeTruthy();
   });
+
+  it('should commit with message', () => {
+    ctx().component.comment.setValue('commit message');
+    ctx().component.commit();
+    expect(ctx().dialogStub.output.value).toEqual('commit message');
+  });
+
+  it('should not commit without message', () => {
+    ctx().component.comment.setValue('');
+    ctx().component.commit();
+    expect(ctx().dialogStub.output.value).toBeUndefined();
+  });
 });
