3e7b51854e63a447a8435fe3b0ef846f44b0d789
veshu <tcveshu@gmail.com>
2019-03-08 14:50:15 +0545

    updated env variables

--- packages/hydration-script/config/default.js
+++ packages/hydration-script/config/default.js
@@ -15,16 +15,16 @@
  */
 
 module.exports = {
-  LOG_LEVEL: process.env.LOG_LEVEL || 'info',
-  ENVIRONMENT_FILE_NAME: process.env.ENVIRONMENT_FILE_NAME || "environments.yaml",
+  LOG_LEVEL: process.env.PERCY_LOG_LEVEL || 'info',
+  ENVIRONMENT_FILE_NAME: process.env.PERCY_ENVIRONMENT_FILE_NAME || "environments.yaml",
   PERCY_CONFIG_FILE_NAME: process.env.PERCY_CONFIG_FILE_NAME || ".percyrc",
   DEFAULT_PERCY_CONFIG: {
-    variablePrefix: process.env.DEFAULT_VARIABLE_PREFIX || "_{",
-    variableSuffix: process.env.DEFAULT_VARIABLE_SUFFIX || "}_",
-    variableNamePrefix: process.env.DEFAULT_VARIABLE_NAME_PREFIX || "$",
+    variablePrefix: process.env.PERCY_DEFAULT_VARIABLE_PREFIX || "_{",
+    variableSuffix: process.env.PERCY_DEFAULT_VARIABLE_SUFFIX || "}_",
+    variableNamePrefix: process.env.PERCY_DEFAULT_VARIABLE_NAME_PREFIX || "$",
   },
-  COLORIZE_CONSOLE: process.env.COLORIZE_CONSOLE ?
-        /(f(?:alse)?|0|off)/gi.test(process.env.COLORIZE_CONSOLE) ? false : true : true,
+  COLORIZE_CONSOLE: process.env.PERCY_CONSOLE_COLORS ?
+        /(f(?:alse)?|0|off)/gi.test(process.env.PERCY_CONSOLE_COLORS) ? false : true : true,
 
 
 };
